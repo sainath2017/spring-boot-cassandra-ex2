194d910cf3356bdba0a9629ee5966b6437f3e564
Mutakoduru Sainath <Mutakodurusainath@gmail.com>
2019-03-06 22:33:55 -0500

    initial commit

---
+++ src/main/java/com/sai/SpringBootCassandraEx2Application.java
@@ -0,0 +1,13 @@
+package com.sai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCassandraEx2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCassandraEx2Application.class, args);
+	}
+
+}
